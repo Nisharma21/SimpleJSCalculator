772e607f3e35655f95aacbe2366dd6f635f4dd56
nisha21096 <nisha.k@datalyticsfoundry.com>
2024-04-24 23:46:46 +0530

    udemy Fundamentals1

---
+++ udemy.js
@@ -0,0 +1,10 @@
+let marksWeights = 78 ;
+let marksHeight = 169 ;
+let BMIMark=weightMark/(heightMark**2);
+console.log(BMIMark);
+let JohnsWeights =92 ;
+let heightJohn=1.95;
+let BMIJohn=weightJohn/(heightJohn**2);
+console.log(BMIMark);
+markHigherBMI=BMIMark>BMIJohn;
+console.log(markHigherBMI);
